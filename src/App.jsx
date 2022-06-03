6527fbfd132e66a5bf1df986c0a61806758b6238
Astrid-Froitier <asfroitier@gmail.com>
2022-06-03 10:14:42 +0200

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Tools from "./components/Tools";
 import "./App.scss";
-// Méthode 1 des images : on importe chaque image
-import dino from "./assets/dinosaur.png";
 
 function App() {
   return (
     <div className="app">
-      <p className="app__text">
-        Ce template est bien configuré pour vos applications front-end avec
-        Vite, Sass et Vercel
-      </p>
-      {/* Méthode 1 : on peut donc directement utiliser son nom */}
-      <img width={150} height={150} src={dino} alt="" />
-      {/* Méthode 2 : après création du dossier "public" à la racine, on peut utiliser le chemin relatif */}
-      <img width={150} height={150} src="./assets/trex.png" alt="" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/tools" element={<Tools />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Tools.jsx
@@ -0,0 +1,24 @@
+import NavBar from "./NavBar";
+
+const Tools = () => {
+  // state
+
+  // fonction
+
+  // debug
+
+  return (
+    <div>
+      <div>
+        <div className="flex justify-between">
+          <div className="w-auto m-auto text-6xl font-ita">Mes outils</div>
+          <div className="">
+            <NavBar />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tools;
