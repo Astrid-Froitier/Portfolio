bc9999fc941cb268e37453e5deb1d0d3acf2e4fb
Astrid-Froitier <asfroitier@gmail.com>
2022-06-29 10:48:48 +0200

    resolved bugfix before my push

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import NavBar from "./NavBar";
 
 const Contact = () => {
@@ -8,14 +9,48 @@ const Contact = () => {
   // debug
 
   return (
-    <div>
-      <div className="flex justify-between">
-        <div className="w-auto m-auto text-6xl font-ita">Contact</div>
-        <div className="">
+    <div className="contact">
+      <div className="contact__header">
+        <div className="contact__header__title">Contact</div>
+        <div>
           <NavBar />
         </div>
       </div>
-      <div>dlfk</div>
+      <div className="contact__text">
+        <div className="contact__text__info">
+          Vous souhaitez me joindre par<p> - téléphone ? 06 75 91 02 55</p>
+          <p>- mail ? asfroitier@gmail.com</p>
+        </div>
+        <div className="contact__text__form">
+          <div>
+            Vous pouvez également m&apos;envoyer un message directement pour me
+            donner votre ressenti :
+          </div>
+          <form className="contact__text__form__complet">
+            <label
+              className="contact__text__form__complet__label"
+              htmlFor="name"
+            >
+              Votre nom :
+            </label>
+            <input type="text" />
+            <label
+              className="contact__text__form__complet__label"
+              htmlFor="socity"
+            >
+              Votre entreprise :
+            </label>
+            <input type="text" />
+            <label
+              className="contact__text__form__complet__label"
+              htmlFor="text"
+            >
+              Votre message :
+            </label>
+            <textarea type="message " />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import NavBar from "./NavBar";
+import { tools, backs, fronts } from "../../data/tools";
 
 const Tools = () => {
   // state
@@ -9,11 +11,28 @@ const Tools = () => {
 
   return (
     <div>
-      <div>
-        <div className="flex justify-between">
-          <div className="w-auto m-auto text-6xl font-ita">Mes outils</div>
-          <div className="">
+      <div className="tools">
+        <div className="tools__header">
+          <h1 className="tools__header__title">Mes outils</h1>
+          <div>
             <NavBar />
+          </div>
+        </div>
+        <div className="tools__map">
+          <div className="tools__map--front">
+            {fronts.map((front) => (
+              <img src={front.img} alt="logo" width={50} height={50} />
+            ))}
+          </div>
+          <div className="tools__map--back">
+            {backs.map((back) => (
+              <img src={back.img} alt="logo" width={50} height={50} />
+            ))}
+          </div>
+          <div className="tools__map--tools">
+            {tools.map((tool) => (
+              <img src={tool.img} alt="logo" width={50} height={50} />
+            ))}
           </div>
         </div>
       </div>
