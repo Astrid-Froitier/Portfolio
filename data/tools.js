a7a717d9fe9e5f432048cbcc2151b352c1505892
Astrid-Froitier <asfroitier@gmail.com>
2022-06-30 23:42:58 +0200

    bugfix on data image

--- data/tools.js
+++ data/tools.js
@@ -2,37 +2,37 @@ const fronts = [
   {
     id: 1,
     name: "HTML 5",
-    img: "../src/assets/logo/html5.svg",
+    img: "/assets/logo/html5.svg",
   },
   {
     id: 2,
     name: "CSS 3",
-    img: "../src/assets/logo/css3.svg",
+    img: "/assets/logo/css3.svg",
   },
   {
     id: 3,
     name: "React",
-    img: "../src/assets/logo/react.svg",
+    img: "/assets/logo/react.svg",
   },
   {
     id: 4,
     name: "JavaScript",
-    img: "../src/assets/logo/javascript.svg",
+    img: "/assets/logo/javascript.svg",
   },
   {
     id: 5,
     name: "Tailwind",
-    img: "../src/assets/logo/tailwind.svg",
+    img: "/assets/logo/tailwind.svg",
   },
   {
     id: 6,
     name: "SASS",
-    img: "../src/assets/logo/sass.svg",
+    img: "/assets/logo/sass.svg",
   },
   {
     id: 7,
     name: "TypeScript",
-    img: "../src/assets/logo/typescript-svgrepo-com.svg",
+    img: "/assets/logo/typescript-svgrepo-com.svg",
   },
 ];
 
@@ -40,47 +40,47 @@ const backs = [
   {
     id: 9,
     name: "Node Js",
-    img: "../src/assets/logo/node.svg",
+    img: "/assets/logo/node.svg",
   },
   {
     id: 10,
     name: "express",
-    img: "../src/assets/logo/express-svgrepo-com.svg",
+    img: "/assets/logo/express-svgrepo-com.svg",
   },
-  { id: 11, name: "mySQL", img: "../src/assets/logo/mysql.svg" },
+  { id: 11, name: "mySQL", img: "/assets/logo/mysql.svg" },
 ];
 
 const tools = [
   {
     id: 8,
     name: "Figma",
-    img: "../src/assets/logo/figma.svg",
+    img: "/assets/logo/figma.svg",
   },
 
   {
     id: 12,
     name: "github",
-    img: "../src/assets/logo/github.svg",
+    img: "/assets/logo/github.svg",
   },
   {
     id: 13,
     name: "trello",
-    img: "../src/assets/logo/trello.svg",
+    img: "/assets/logo/trello.svg",
   },
   {
     id: 14,
     name: "NPM",
-    img: "../src/assets/logo/npm.svg",
+    img: "/assets/logo/npm.svg",
   },
   {
     id: 15,
     name: "postman",
-    img: "../src/assets/logo/postman.svg",
+    img: "/assets/logo/postman.svg",
   },
   {
     id: 16,
     name: "VS code",
-    img: "../src/assets/logo/vscode.svg",
+    img: "/assets/logo/vscode.svg",
   },
 ];
 
