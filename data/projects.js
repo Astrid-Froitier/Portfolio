1a01e452f99e9b84aace7c8a0fbffc849c9daf06
Astrid-Froitier <asfroitier@gmail.com>
2023-01-13 11:01:09 +0100

    added certificate name and resolved bugfix on responsive css

--- data/projects.js
+++ data/projects.js
@@ -4,7 +4,7 @@ const projects = [
     url: "https://astrid-froitier.github.io/Tiny-Houses/",
     img: "/assets/tinyHouseProject3.jpeg",
     tools: "HTML, SASS",
-    desc: "Il s'agit du premier projet sur lequel j'ai travaillé au début de ma première formation. Nous l'avons réaliser en deux semaines.",
+    desc: "Il s'agit du premier projet sur lequel j'ai travaillé au début de ma première formation en mars 2022 à la Wild code school de Biarritz. Nous l'avons réalisé en deux semaines.",
   },
   {
     title: "Star Tour",
@@ -18,13 +18,13 @@ const projects = [
     url: "https://pocli.vercel.app",
     img: "/assets/PoCLi copie.png",
     tools: "React Js, Sass, Typescript, nodeJs, Express, MySQL",
-    desc: "Troisième et dernier projet de ma formaiton. Site complètement FullStack réalisé pour le compte d'une association qui à pour volonter de créer du lien entre les personnes et sur le territoire. A ce jour le site est toujours en attente de déploiement.",
+    desc: "Troisième et dernier projet de ma première formaiton. Site complètement FullStack réalisé pour le compte d'une association. Cette associoation, Pocli, à pour volonté de créer du lien entre les personnes et sur le territoire. A ce jour le site est toujours en attente de déploiement.",
   },
   {
     title: "Ears Up",
     img: "/assets/earsUp.png",
     tools: "Symphony, Twig, Bootstrap",
-    desc: "Projet réalisé durant ma deuxième formation en l'espace de deux semaines. Nous avons réalisé une platforme de partage de musiques libre de droit.",
+    desc: "Projet réalisé durant ma deuxième formation en l'espace de deux semaines. Nous avons réalisé une platforme de partage de musiques libre de droit. Il n'est pas encore déployé pour le moment.",
   },
 ];
 
